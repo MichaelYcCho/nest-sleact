545be5511099b2c678c10d96683fdb62d49ea693
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-10-13 05:08:19 +0900

    feat: Get방식 Swagger Example

--- src/dms/dms.controller.ts
+++ src/dms/dms.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
 
-@Controller('dms')
-export class DmsController {}
+@ApiTags('DMS')
+@Controller('api/workspaces')
+export class DmsController {
+  @ApiParam({
+    name: 'url',
+    required: true,
+    description: '워크스페이스 url',
+  })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: '사용자 id',
+  })
+  @ApiQuery({
+    name: 'perPage',
+    required: true,
+    description: '한번에 가져오는 개수',
+  })
+  @ApiQuery({
+    name: 'page',
+    required: true,
+    description: '불러올 페이지',
+  })
+  @Get(':url/dms/:id/chats')
+  getChat(@Query() query, @Param() param) {
+    console.log(query.perPage, query.page);
+    console.log(param.id, param.url);
+  }
+}
